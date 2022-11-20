faf0d415a3a0c16ff7a84db9f3a062b676f81a02
m.tatybaev <m.tatybaev@qiwi.uz>
2022-11-20 22:20:18 +0500

    parse_data via API

--- Dockerfile
+++ Dockerfile
@@ -11,10 +11,8 @@ ENV PYTHONUNBUFFERED=1
 
 # Install pip requirements
 COPY requirements.txt .
+RUN python -m pip install --upgrade pip
 RUN python -m pip install -r requirements.txt
 
 WORKDIR /app
 COPY . /app
-
-# During debugging, this entry point will be overridden. For more information, please refer to https://aka.ms/vscode-docker-python-debug
-# CMD ["gunicorn", "--bind", "0.0.0.0:5000", "app:app"]
--- modules/main.py
+++ modules/main.py
@@ -1,13 +1,38 @@
 """Main module"""
+import requests
+
 from flask import Blueprint, jsonify, request
 
-main_bp = Blueprint('main', __name__)
+main_bp = Blueprint("main", __name__)
 
-@main_bp.route('/first', methods=["GET"])
+@main_bp.route("/first", methods=["GET"])
 def first():
     """First Endpoint"""
     name = request.args.get("name")
     data = {
         "name": f"Hello {name}"
     }
+    return jsonify(data)
+
+@main_bp.route("/parse_data", methods=["GET"])
+def parse_data():
+    """Parse data from API"""
+    headers = {
+        "apikey": "b0530240-68f3-11ed-833f-6d356bd82086"
+    }
+
+    params = (
+        ("continent","Europe"),
+    )
+
+    response = requests.get(
+        'https://app.sportdataapi.com/api/v1/soccer/countries',
+        headers=headers,
+        params=params
+    )
+
+    data = {
+        "data": response.json()
+    }
+
     return jsonify(data)
